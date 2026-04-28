564002179de2ebe87aa2c71ef3659e347d81c62d
kmds <kgraven2009@gmail.com>
2026-04-28 15:36:19 +0300

    Все

---
+++ Selection_sort.py
@@ -0,0 +1,18 @@
+def list_elements(numb):
+    list_tot = []
+    for i in range(numb):
+        list_tot.append(i+1)
+    return list_tot
+def sort(amount):
+    a = list_elements(amount)
+    for i in range(0, len(a) - 1):
+        m = max(a[i:])
+        ind = a.index(m)
+        a[i], a[ind] = a[ind], a[i]
+    print(a)
+sort(100)
+sort(300)
+sort(500)
+sort(1000)
+sort(1500)
+sort(2000
---
+++ algoritms.py
@@ -0,0 +1,10 @@
+
+def selection_sort_desc(arr):
+    n = len(arr)
+    for i in range(n):
+        max_idx = i
+        for j in range(i + 1, n):
+            if arr[j] > arr[max_idx]:
+                max_idx = j
+        arr[i], arr[max_idx] = arr[max_idx], arr[i]
+    return arr
